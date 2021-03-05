44ea2cc63f2969c217fda78fbc91029d7dc779d3
Taylor Abbott <68927611+taylornicole906@users.noreply.github.com>
2021-03-05 09:54:28 -0800

    fixed media query

---
+++ build/precache-manifest.5a95e490485a0098e5e522eeae6e24d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30ee8ff63138203b07e9a7edc0421883",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6e65b73dfae6850ba57",
+    "url": "/static/css/main.6577fa23.chunk.css"
+  },
+  {
+    "revision": "8874131a37d8001a8181",
+    "url": "/static/js/2.4f3b5a96.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/static/js/2.4f3b5a96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6e65b73dfae6850ba57",
+    "url": "/static/js/main.8665ab83.chunk.js"
+  },
+  {
+    "revision": "142b697ed77b435eaccc",
+    "url": "/static/js/runtime-main.efa8e349.js"
+  }
+]);
